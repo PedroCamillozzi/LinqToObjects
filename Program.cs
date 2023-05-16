38e678803a3d536a73e335f5ce8d95a83bb11030
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-05-16 20:54:31 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace LinqToObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList provinciasArgentina = new ArrayList()
+        {
+            "Buenos Aires",
+            "Catamarca",
+            "Chaco",
+            "Chubut",
+            "Córdoba",
+            "Corrientes",
+            "Entre Ríos",
+            "Formosa",
+            "Jujuy",
+            "La Pampa",
+            "La Rioja",
+            "Mendoza",
+            "Misiones",
+            "Neuquén",
+            "Río Negro",
+            "Salta",
+            "San Juan",
+            "San Luis",
+            "Santa Cruz",
+            "Santa Fe",
+            "Santiago del Estero",
+            "Tierra del Fuego",
+            "Tucumán"
+        };
+
+            var misProvincias = provinciasArgentina.Cast<string>().Where(p => p.StartsWith("S") || p.StartsWith("T"));
+
+            foreach (var p in misProvincias)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
